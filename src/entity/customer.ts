1f5e8b1182535e4fb6f80264a5aae1329d140999
Isac Sousa <isacsousadev@gmail.com>
2022-04-19 12:00:50 -0300

    refatora customer para uma entidade rica

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,31 +1,38 @@
+// ENTITY -> REGRAS DE NEGÓCIO (Complexidade de negócio);
+// ENTITY / MODEL -> Atender a infraestrutura (ORM) (Complexidade acidental);
+
 class Customer {
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string, address: string = "") {
         this._id = id;
         this._name = name;
         this._address = address;
+
+        if(address.length !== 0) this.activate();
+
+        this.validate();
     }
 
-    get id(): string {
-        return this._id;
+    validate() {
+        if(this._id.length === 0) throw new Error("Id is required"); 
+        if(this._name.length === 0) throw new Error("Name is required");    
     }
 
-    get name(): string {
-        return this._name;
-    }
-
-    set name(name: string) {
+    changeName(name: string): void {
         this._name = name;
+        this.validate();
     }
 
-    get address(): string {
-        return this._address;
+    activate(): void {
+        if(this._address.length === 0) throw new Error("Address is mandatory to activate a customer");
+        this._active = true;
     }
 
-    set address(address: string) {
-        this._address = address;
+    deactivate(): void {
+        this._active = false;
     }
 }
