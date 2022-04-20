9cbd26123a5144a8edfb201930e6cc18e385eeab
Isac Sousa <isacsousadev@gmail.com>
2022-04-20 11:57:49 -0300

    define dois agregados, sendo um para customer e um para order

---
+++ src/entity/order.ts
@@ -0,0 +1,22 @@
+import OrderItem from "./order_item";
+
+export default class Order {
+    _id: string;
+    _customerId: string;
+    _items: OrderItem[];
+
+    constructor(id: string, customerId: string, items: OrderItem[]) {
+        this._id = id;
+        this._customerId = customerId;
+        this._items = items;
+        
+        this.validate();
+    }
+
+
+    validate() {
+        if(this._id.length === 0) throw new Error("Id is required");
+        if(this._customerId.length === 0) throw new Error("CustomerId is required");
+        if(this._items.length < 1) throw new Error("Itens need to have at least 1 item");
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Address from "./entity/address";
+import Customer from "./entity/customer";
+import Order from "./entity/order";
+import OrderItem from "./entity/order_item";
+
+// agregago de customer
+let customer = new Customer("123", "Isac Sousa");
+const address = new Address("Rua do frango", 10, "12345-678", "Mossoró");
+
+customer.Address = address;
+customer.activate();
+
+
+// agregago de order
+let item1 = new OrderItem("1", "item 1", 10);
+let item2 = new OrderItem("2", "item 2", 20);
+
+const order = new Order("1", "123", [item1, item2]);
