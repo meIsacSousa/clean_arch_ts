ab4da3447c494be423dff34b5766acc4f79df976
Isac Sousa <isacsousadev@gmail.com>
2022-04-20 13:33:17 -0300

    inclui configurações iniciais para testes

---
+++ src/entity/customer.spec.ts
@@ -0,0 +1,6 @@
+describe("Customer unit tests", () => {
+    it("Should get 1 as result", () => {
+        const result = 1;
+        expect(result).toBe(1);
+    });
+});
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -13,10 +13,13 @@ export default class Order {
         this.validate();
     }
 
+    total(): number {
+        return this._items.reduce((acc, item) => acc + item._price, 0);
+    }
 
     validate() {
         if(this._id.length === 0) throw new Error("Id is required");
         if(this._customerId.length === 0) throw new Error("CustomerId is required");
-        if(this._items.length < 1) throw new Error("Itens need to have at least 1 item");
+        if(this._items.length < 1) throw new Error("Items need to have at least 1 item");
     }
 }
