aeaaf53db1909d95046014370c7208f3056ba45c
Isac Sousa <isacsousadev@gmail.com>
2022-04-28 14:55:27 -0300

    cria testes para orderService

---
+++ src/service/order.service.spec.ts
@@ -0,0 +1,18 @@
+import Order from "../entity/order";
+import OrderItem from "../entity/order_item";
+import OrderService from "./order.service";
+
+describe("Order service unit teste", () => {
+    it("Should get total of all orders", () => {
+        const orderItem1 = new OrderItem("orit1", "first order", 20, "pr1", 1);
+        const orderItem2 = new OrderItem("orit2", "second order", 25, "pr2", 2);
+        const orderItem3 = new OrderItem("orit3", "third order", 30, "pr3", 1);
+        const orderItem4 = new OrderItem("orit4", "fourth order", 35, "pr4", 2);
+
+        const order1 = new Order("or1", "cus1", [orderItem1, orderItem2]);
+        const order2 = new Order("or2", "cus2", [orderItem3, orderItem4]);
+
+        const total = OrderService.getTotal([order1, order2]);
+        expect(total).toBe(170);
+    });
+});
