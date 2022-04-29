4b4489602379dc77c3b4a360abc014d20546d2b0
Isac Sousa <isacsousadev@gmail.com>
2022-04-29 13:17:46 -0300

    adiciona configuração de swcrc para reconhecer os decorators durante os testes

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,15 +1,19 @@
 import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../db/sequelize/model/product.model";
 
 describe("Product Repository unit tests", () => {
     let sequelize: Sequelize;
 
-    beforeEach(() => {
+    beforeEach(async () => {
         sequelize = new Sequelize({
             dialect: 'sqlite',
             storage: ':memory:',
             logging: false,
             sync: { force: true }
         });
+        
+        sequelize.addModels([ProductModel]);
+        await sequelize.sync();
     });
 
     afterEach(async () => {
