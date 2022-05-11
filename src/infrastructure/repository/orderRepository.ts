3cf6f7d388b903fbac3b6544e09b181e8381bf15
Isac Sousa <isacsousadev@gmail.com>
2022-05-10 22:43:03 -0300

    ajuste em método de update ainda não testado

--- src/infrastructure/repository/orderRepository.ts
+++ src/infrastructure/repository/orderRepository.ts
@@ -6,21 +6,19 @@ import OrderItemModel from "../db/sequelize/model/orderItem.model";
 
 export default class OrderRepository implements IOrderRepository {
     async update(entity: Order): Promise<void> {
-        await OrderModel.update({
-            customerId: entity.customerId,
-            total: entity.total(),
-            items: entity.items.map(i => ({
-                id: i.id,
-                name: i.name,
-                price: i.price,
-                productId: i.productId,
-                quantity: i.quantity,
-            }))
-        }, { where: { id: entity.id } });
+        const orderModel = await OrderModel.findOne({ where: { id: entity.id }, include: [{ model: OrderItemModel }] });
+        if (orderModel) {
+            orderModel.update({
+                id: entity.id,
+                customerId: entity.customerId,
+                total: entity.total(),
+                items: this.orderItemToModelMapper(entity.items)
+            });
+        }
     }
 
     async find(id: string): Promise<Order> {
-        const orderModel = await OrderModel.findOne({ where: { id } });
+        const orderModel = await OrderModel.findOne({ where: { id }, include: [{ model: OrderItemModel }] });
 
         if (orderModel) {
             const items = orderModel.items.map(i => new OrderItem(i.id, i.name, i.price, i.productId, i.quantity));
@@ -31,7 +29,7 @@ export default class OrderRepository implements IOrderRepository {
     }
 
     async findAll(): Promise<Order[]> {
-        const orderModel = await OrderModel.findAll();
+        const orderModel = await OrderModel.findAll({ include: [{ model: OrderItemModel }] });
 
         return orderModel.map(o => {
             const items = o.items.map(i => new OrderItem(i.id, i.name, i.price, i.productId, i.quantity));
@@ -44,13 +42,18 @@ export default class OrderRepository implements IOrderRepository {
             id: entity.id,
             customerId: entity.customerId,
             total: entity.total(),
-            items: entity.items.map(i => ({
-                id: i.id,
-                name: i.name,
-                price: i.price,
-                productId: i.productId,
-                quantity: i.quantity,
-            }))
+            items: this.orderItemToModelMapper(entity.items)
         }, { include: [{ model: OrderItemModel }] });
+    }
+
+
+    private orderItemToModelMapper(items: OrderItem[]): OrderItemModel[] {
+        return items.map(i => ({
+            id: i.id,
+            name: i.name,
+            price: i.price,
+            productId: i.productId,
+            quantity: i.quantity,
+        } as OrderItemModel))
     }
 }
